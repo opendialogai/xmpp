e4eeb0703495a348201c399c78c7bbccea24815f
Steve McDougall <juststeveking@gmail.com>
2019-09-20 10:10:51 +0100

    Removing LogHelper from Service provider as it is not needed

--- src/XmppServiceProvider.php
+++ src/XmppServiceProvider.php
@@ -4,24 +4,16 @@ declare(strict_types=1);
 
 namespace OpenDialogAi\Xmpp;
 
-use Illuminate\Support\Facades\Log;
-use OpenDialogAi\Core\LoggingHelper;
 use Illuminate\Support\ServiceProvider;
-use OpenDialogAi\ResponseEngine\Service\ResponseEngineService;
-use OpenDialogAi\ResponseEngine\Service\ResponseEngineServiceInterface;
 use OpenDialogAi\SensorEngine\SensorInterface;
 use OpenDialogAi\SensorEngine\Service\SensorService;
-use OpenDialogAi\Xmpp\SensorEngine\Http\Requests\IncomingXmppMessage;
-use OpenDialogAi\Core\Http\Middleware\RequestLoggerMiddleware;
+use OpenDialogAi\ResponseEngine\Service\ResponseEngineService;
 use OpenDialogAi\SensorEngine\Contracts\IncomingMessageInterface;
+use OpenDialogAi\Xmpp\SensorEngine\Http\Requests\IncomingXmppMessage;
+use OpenDialogAi\ResponseEngine\Service\ResponseEngineServiceInterface;
 
 class XmppServiceProvider extends ServiceProvider
 {
-    /**
-     * @var string $requestId
-     */
-    private $requestId;
-
     public function boot()
     {
         $this->publishes([
@@ -31,13 +23,6 @@ class XmppServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-
-        $this->requestId = uniqid('od-', true);
-        $this->app->when(RequestLoggerMiddleware::class)
-            ->needs('$requestId')
-            ->give($this->requestId);
-
-        Log::pushProcessor(LoggingHelper::getLogUserIdProcessor($this->requestId));
     }
 
     public function register()
