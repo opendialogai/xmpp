7210abc296e8bb70afd847422009937b8ec58cfd
Steve McDougall <juststeveking@gmail.com>
2019-09-20 13:42:55 +0100

    Wrapping up some tests on sensor checks - early stage checks only

--- tests/SensorEngine/Sensors/XmppSensorTest.php
+++ tests/SensorEngine/Sensors/XmppSensorTest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace OpenDialogAi\Xmpp\Tests\SensorEngine\Sensors;
 
 use Illuminate\Http\Request;
+use OpenDialogAi\Core\Utterances\Exceptions\UtteranceUnknownMessageType;
 use OpenDialogAi\Xmpp\SensorEngine\Sensors\XmppSensor;
+use OpenDialogAi\Xmpp\Utterances\Xmpp\TextUtterance;
 
 class XmppSensorTest extends XmppSensorTestBase
 {
@@ -32,6 +34,32 @@ class XmppSensorTest extends XmppSensorTestBase
 
         $this->assertCount(count($data), $utterance->getData());
         $this->assertEquals($data, $utterance->getData());
+    }
+
+    public function testSensorThrowsExceptionOnWrongType()
+    {
+        $this->expectException(UtteranceUnknownMessageType::class);
+
+        $data = [
+            'text' => 'A message'
+        ];
+
+        $body = $this->generateResponseMessage('not-allowed', $data);
+
+        $utterance = $this->sensor->interpret(new Request($body));
+    }
+
+    public function testUtteranceIsCorrectType()
+    {
+        $data = [
+            'text' => 'A message'
+        ];
+
+        $body = $this->generateResponseMessage('text', $data);
+
+        $utterance = $this->sensor->interpret(new Request($body));
+
+        $this->assertInstanceOf(TextUtterance::class, $utterance);
     }
 
     public function testSensorReturnsAnUtteranceWithCorrectUserData()
