348d96b07ec9169f1b1172a55a0b8aff1424d906
MarioFrisuelos <mfrisuelos@gmail.com>
2020-10-16 16:51:28 +0100

    Update XmppMessageFormatter with the new message formatter interface methods

--- src/ResponseEngine/Message/XmppMessageFormatter.php
+++ src/ResponseEngine/Message/XmppMessageFormatter.php
@@ -5,12 +5,18 @@ namespace OpenDialogAi\Xmpp\ResponseEngine\Message;
 use Exception;
 use Illuminate\Support\Facades\Log;
 use OpenDialogAi\ResponseEngine\Formatters\BaseMessageFormatter;
+use OpenDialogAi\ResponseEngine\Message\AutocompleteMessage;
 use OpenDialogAi\ResponseEngine\Message\ButtonMessage;
+use OpenDialogAi\ResponseEngine\Message\DatePickerMessage;
 use OpenDialogAi\ResponseEngine\Message\EmptyMessage;
 use OpenDialogAi\ResponseEngine\Message\FormMessage;
+use OpenDialogAi\ResponseEngine\Message\FullPageFormMessage;
+use OpenDialogAi\ResponseEngine\Message\FullPageRichMessage;
+use OpenDialogAi\ResponseEngine\Message\HandToSystemMessage;
 use OpenDialogAi\ResponseEngine\Message\ImageMessage;
 use OpenDialogAi\ResponseEngine\Message\ListMessage;
 use OpenDialogAi\ResponseEngine\Message\LongTextMessage;
+use OpenDialogAi\ResponseEngine\Message\MetaMessage;
 use OpenDialogAi\ResponseEngine\Message\OpenDialogMessage;
 use OpenDialogAi\ResponseEngine\Message\OpenDialogMessages;
 use OpenDialogAi\ResponseEngine\Message\RichMessage;
@@ -53,41 +59,6 @@ class XmppMessageFormatter extends BaseMessageFormatter
     {
         $message = (new XmppMessage())->setText($template[self::TEXT], [], true);
         return $message;
-    }
-
-    public function generateButtonMessage(array $template): ButtonMessage
-    {
-        return null;
-    }
-
-    public function generateEmptyMessage(): EmptyMessage
-    {
-        return null;
-    }
-
-    public function generateFormMessage(array $template): FormMessage
-    {
-        return null;
-    }
-
-    public function generateImageMessage(array $template): ImageMessage
-    {
-        return null;
-    }
-
-    public function generateListMessage(array $template): ListMessage
-    {
-        return null;
-    }
-
-    public function generateLongTextMessage(array $template): LongTextMessage
-    {
-        return null;
-    }
-
-    public function generateRichMessage(array $template): RichMessage
-    {
-        return null;
     }
 
     /**
@@ -136,5 +107,70 @@ class XmppMessageFormatter extends BaseMessageFormatter
         }
 
         return trim($text);
+    }
+
+    public function generateAutocompleteMessage(array $template): AutocompleteMessage
+    {
+        // TODO: Implement generateAutocompleteMessage() method.
+    }
+
+    public function generateFullPageFormMessage(array $template): FullPageFormMessage
+    {
+        // TODO: Implement generateFullPageFormMessage() method.
+    }
+
+    public function generateMetaMessage(array $template): MetaMessage
+    {
+        // TODO: Implement generateMetaMessage() method.
+    }
+
+    public function generateFullPageRichMessage(array $template): FullPageRichMessage
+    {
+        // TODO: Implement generateFullPageRichMessage() method.
+    }
+
+    public function generateHandToSystemMessage(array $template): HandToSystemMessage
+    {
+        // TODO: Implement generateHandToSystemMessage() method.
+    }
+
+    public function generateDatePickerMessage(array $template): DatePickerMessage
+    {
+        // TODO: Implement generateDatePickerMessage() method.
+    }
+
+    public function generateButtonMessage(array $template): ButtonMessage
+    {
+        // TODO: Implement generateButtonMessage() method.
+    }
+
+    public function generateEmptyMessage(): EmptyMessage
+    {
+        // TODO: Implement generateEmptyMessage() method.
+    }
+
+    public function generateFormMessage(array $template): FormMessage
+    {
+        // TODO: Implement generateFormMessage() method.
+    }
+
+    public function generateImageMessage(array $template): ImageMessage
+    {
+        // TODO: Implement generateImageMessage() method.
+    }
+
+    public function generateListMessage(array $template): ListMessage
+    {
+        // TODO: Implement generateListMessage() method.
+    }
+
+    public function generateLongTextMessage(array $template): LongTextMessage
+    {
+        // TODO: Implement generateLongTextMessage() method.
+    }
+
+    public function generateRichMessage(array $template): RichMessage
+    {
+        // TODO: Implement generateRichMessage() method.
     }
 }
